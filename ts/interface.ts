475b1c18d1942229bb1329a07eca2af03cef249e
king <2075259561@qq.com>
2020-11-10 18:12:57 +0800

    2020-11-10

--- ts/interface.ts
+++ ts/interface.ts
@@ -29,6 +29,7 @@ interface optional {
     color?: String,
     height?: String
 }
+
 /*
 * 预定义参数 需要return
 * */
@@ -46,10 +47,66 @@ function square(config: optional): { color: String; area: Number } {
     return data;
 }
 
-console.log(square({color:'color'}));
-console.log(square({height:'height'}));
-console.log(square({color:'color',height:'height'}));
+console.log(square({color: 'color'}));
+console.log(square({height: 'height'}));
+console.log(square({color: 'color', height: 'height'}));
 
 /*
 * 只读属性
 * */
+
+/*
+* 函数类型
+* */
+interface function_i {
+    (a: string, b: number): Boolean;
+}
+
+let fun: function_i;
+fun = function (a: String, b: Number) {
+    console.log(a + '-' + b);
+    console.log(a > b + '');
+    return a > b + '';
+};
+fun('aaa', 999999);
+
+/*
+* 可索引的类型
+* */
+interface StringArray {
+    [index: number]: string
+}
+
+let myArray: StringArray;
+myArray = ['AAA', 'BBB'];
+
+let str = myArray['0'];
+console.log(str);
+
+
+/*
+* 类类型
+* 实现接口
+* */
+interface clockInterface {
+    currentTime: string;
+
+    setTime(d: string);
+}
+
+class Clock implements clockInterface {
+    currentTime: string;
+
+    setTime(d: string) {
+        this.currentTime = d;
+    }
+
+    constructor(m: string) {
+        console.log(m);
+    }
+}
+
+let ll = new Clock('********');
+ll.currentTime='222';
+ll.setTime('lll');
+console.log(ll.setTime('[[['))
