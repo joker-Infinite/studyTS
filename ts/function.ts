a373e30ee5fff39efd9910fb4e8ad1b3b3efbf3a
king <2075259561@qq.com>
2020-12-21 18:06:26 +0800

    class

--- ts/function.ts
+++ ts/function.ts
@@ -1,3 +1,17 @@
+//函数名括号后跟了数据类型就需要 return
 function fun(x: number, y: number): number {
-    return 3
+    return x + y;
 }
+
+fun(3, 5);
+
+let nFun: (x: string, y: string) => string = function (x: string, y: string): string {
+    console.log(x + y);
+    return x + y;
+};
+nFun('1', '2');
+//推断函数
+
+let ff = function (x: string, y: string): string {
+    return x + y;
+};
